2288275a1f83bac4fedd73b00b0df724929a96f0
Rafael Souza <rafael.jose@ferreri.co>
2023-10-03 21:29:05 -0300

    boom2

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,26 @@
+import { useContext } from 'react';
 import{ View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { AuthContext } from '../../contexts/auth';
+import { deleteItemAsync } from "expo-secure-store";
+import { router } from 'expo-router';
 
 export default function Profile() {
+    const { setToken } = useContext(AuthContext)
+
+    async function handleLogout() {
+        setToken(null)
+        await deleteItemAsync("token");
+
+        router.push('/')
+    }
+
     return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
             <Text>Profile</Text>
+            <TouchableOpacity onPress={handleLogout}>
+                Sair do Software
+            </TouchableOpacity>
         </View>
     )
 }
